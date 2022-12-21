b466d0b7090af1a0735873df442efc5bdc40973d
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2022-12-21 17:05:57 +0530

    Duplicate IN array Traditional method

---
+++ Arrays/4_FindDuplicate.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int duplicate(int arr[],int size){
+
+    sort(arr,arr+size);
+
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==arr[i+1])
+        {
+            return arr[i];
+        }
+    }
+
+
+    return 0;
+
+
+}
+
+
+
+int main()
+{
+    int arr[5]={10,20,30,40,30};
+
+    int k=duplicate(arr,5);
+
+    cout<<k;
+return 0;
+}
