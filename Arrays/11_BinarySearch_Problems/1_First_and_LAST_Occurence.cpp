2b5ac77f282bcec9c14c692b90881c7b6b0b51c0
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2022-12-22 15:12:44 +0530

    Selection Sorting

---
+++ Arrays/11_BinarySearch_Problems/1_First_and_LAST_Occurence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+
+
+
+
+
+int main()
+{
+int arr[]={1,2,3,4,4,4,5,6,7};
+
+int size=sizeof(arr)/sizeof(arr[0]);
+
+int key=4;
+
+vector<int> v;
+
+for(int i=0;i<size;i++)
+{
+    int k=i;
+    while(arr[i]==key)
+    {
+        
+
+    }
+
+}
+    
+    for(auto i:v)
+    cout<<i<<" ";
+return 0;
+}
---
+++ Arrays/Sorting/1_Selection_Sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void SelectionSort(int arr[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j ++)
+        {
+
+            if (arr[minIndex] > arr[j])
+                minIndex = j;
+        }
+        swap(arr[minIndex],arr[i]);
+        cout<<"done"<<" ";
+    }
+
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
+
+int main()
+{
+    int arr[] = {1, 3, 4, 5, 6, 8, 6, 5121, 4, 0};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout<<"hello";
+
+    SelectionSort(arr, size);
+
+    return 0;
+}
