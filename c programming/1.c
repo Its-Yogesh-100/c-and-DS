83b2313e69c4260584bfe6415e01960a189b1935
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-01-09 17:49:13 +0530

    c

--- c programming/1.c
+++ c programming/1.c
@@ -6,8 +6,8 @@ int main()
 
     int arr[]={10,20,30,40};
 
-     printf("%d",&arr);
-    printf("%s","hello world");
+ int   k=  printf("%d\n",&arr);
+    printf("hello world");
 
       return 0;
 }
