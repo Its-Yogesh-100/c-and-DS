e342acd0a8cb5c58d1b28bc600915913ea02240c
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-01-19 22:09:26 +0530

    about the class

---
+++ OOPS/class.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+// class is nothing but a template that helps to create an
+// instance of objects in a memeory
+
+// objects are the real world entities
+
+class Hero{
+
+     
+};
+int main()
+{
+  Hero h1;
+  
+  cout<<h1.health<<endl;
+  cout<<h1.level<<endl;
+return 0;
+}
