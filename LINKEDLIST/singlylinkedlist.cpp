43104e13e06f4eb468d0aaffe90bc7c7fb722662
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-01-20 00:23:38 +0530

    Creation LINKEDLIST

---
+++ LINKEDLIST/singlylinkedlist.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+// 
+
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+int main()
+{
+  Node *node1=new Node();
+  cout<<node1->data<<endl;
+  cout<<node1->next;
+    
+return 0;
+} 
