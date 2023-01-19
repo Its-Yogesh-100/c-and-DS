4760213adf2454615b8b05c228ed8929c52ba4dc
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-01-19 22:18:29 +0530

    getter and setter in c++

--- OOPS/class.cpp
+++ OOPS/class.cpp
@@ -1,20 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-    
+
 // class is nothing but a template that helps to create an
 // instance of objects in a memeory
 
 // objects are the real world entities
 
-class Hero{
+// ACCESS MODIFIERS
 
-     
+// BY DEFAULT PRIVATE ARE THERE
+
+// GETTERS AND SETTERS USED TO ACCESS
+// VARIABLE OUTHSIDE THE CLASS
+
+class Hero
+{
+
+private:
+  int health;
+
+public:
+  int level;
+  void print()
+  {
+    cout << "HELLO";
+  }
 };
 int main()
 {
   Hero h1;
-  
-  cout<<h1.health<<endl;
-  cout<<h1.level<<endl;
-return 0;
+
+  cout << h1.health << endl;
+  cout << h1.level << endl;
+  return 0;
 }
