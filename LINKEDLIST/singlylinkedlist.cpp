8b439dc4cbc9e55d8c91ebd8b98951e4965d23fd
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-01-20 01:00:14 +0530

    Creation insertionat frist printing

--- LINKEDLIST/singlylinkedlist.cpp
+++ LINKEDLIST/singlylinkedlist.cpp
@@ -16,11 +16,44 @@ class Node{
     }
 };
 
+
+void InsertAtHead(Node* &head,int data)
+{
+    // creating a newnode;
+
+    Node* temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+
+void print(Node* &head)
+{
+   // creating a new pointer
+   Node * temp=head;
+
+   while(temp!=NULL){
+
+    cout<<temp->data<<" ";
+    temp=temp->next;
+   }
+   cout<<endl;
+ 
+}
+
 int main()
 {
-  Node *node1=new Node();
-  cout<<node1->data<<endl;
-  cout<<node1->next;
+
+// creating a new node
+  Node *node1=new Node(20);
+// head pointed to node1
+  Node* head=node1;
+
+  print(head);
+
+  InsertAtHead(head,30);
+  print(head);
+
+ 
     
 return 0;
 } 
